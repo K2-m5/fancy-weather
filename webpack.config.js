04fb93b0f6f669de514ed7b86cb3bbeead8f6d44
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-05-30 13:39:26 +0300

    init: init task fancy-weather

---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {}
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const jsLoader = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        '@babel/preset-env'
+      ]
+    }
+  }]
+
+  return loaders
+}
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    index: ['@babel/polyfill','./index.js']
+  },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    port: 3000,
+    hot: isDev,
+    contentBase: path.join(__dirname, 'dist')
+  },
+  devtool: isDev ? 'source-map' : '',
+  optimization: optimization(),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@modules': path.resolve(__dirname, 'src/components')
+    }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    }),
+    new CopyWebpackPlugin([
+      { from: 'assets', to: 'assets' }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            hmr: isDev,
+            reloadAll: true
+          }
+        }, 'css-loader']
+      },
+      {
+        test: /\.jpg$|\.svg$/,
+        loader: 'url-loader',
+        options: {
+          limit: false,
+        },
+      },
+      {
+        test: /\.js$/,
+        use: jsLoader()
+      }
+    ]
+  }
+}
