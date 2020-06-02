580ab621967c1b40b42e48e9048eaf452193ced2
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-06-03 00:42:10 +0300

    refactor: refactor mapkUp

--- src/components/BuildDom/BuildDom.js
+++ src/components/BuildDom/BuildDom.js
@@ -88,25 +88,79 @@ export class BuildDom {
     const weatherTodayTemp = createElement('div', 'text_base', 'weather_today_block--temp')
     const weatherTodayImg = createElement('img', 'weather_today_block--sign')
 
+    const weatherTodayInfoList = createElement('div', 'weather_today_block--info_list')
+    const weatherTodayInfoListItem1 = createElement('div', 'text_base', 'weather_today_block--info_list-item')
+    const weatherTodayInfoListItem2 = createElement('div', 'text_base', 'weather_today_block--info_list-item')
+    const weatherTodayInfoListItem3 = createElement('div', 'text_base', 'weather_today_block--info_list-item')
+    const weatherTodayInfoListItem4 = createElement('div', 'text_base', 'weather_today_block--info_list-item')
+
     const weatherNextDayList = createElement('div', 'weather_days_block');
     const weatherNextDay1 = createElement('div', 'day_block');
+    const weatherNextDayWeak1 = createElement('div', 'text_base', 'day_block--name');
+    const weatherNextDay1Temp = createElement('div', 'text_base', 'day_block--temp');
+    const weatherNextDay1Img =createElement('img', 'day_block--sign')
+
     const weatherNextDay2 = createElement('div', 'day_block');
+    const weatherNextDayWeak2 = createElement('div', 'text_base', 'day_block--name');
+    const weatherNextDay2Temp = createElement('div', 'text_base', 'day_block--temp');
+    const weatherNextDay2Img =createElement('img', 'day_block--sign')
+
     const weatherNextDay3 = createElement('div', 'day_block');
+    const weatherNextDayWeak3 = createElement('div', 'text_base', 'day_block--name');
+    const weatherNextDay3Temp = createElement('div', 'text_base', 'day_block--temp');
+    const weatherNextDay3Img =createElement('img', 'day_block--sign')
+
     const mapBlock = createElement ('div', 'map-block');
     const map = createElement ('div', 'map');
     map.id = 'map';
-    weatherTodayImg.setAttribute('src', 'assets/img/rain.png')
+    weatherTodayImg.setAttribute('src', 'assets/img/rain.png');
+    weatherNextDay1Img.setAttribute('src', 'assets/img/rain.png');
+    weatherNextDay2Img.setAttribute('src', 'assets/img/rain.png');
+    weatherNextDay3Img.setAttribute('src', 'assets/img/rain.png');
 
     weatherCity.innerText = 'Belarus, Minsk';
     weatherDate.innerText = toString(new Date);
     weatherTodayTemp.innerText = '+54';
-    weatherNextDay1.innerText = '+55';
-    weatherNextDay2.innerText = '+55';
-    weatherNextDay3.innerText = '+55';
+    weatherNextDay1Temp.innerText = '+55';
+    weatherNextDay2Temp.innerText = '+55';
+    weatherNextDay3Temp.innerText = '+55';
+    weatherNextDayWeak1.innerText = 'Monday';
+    weatherNextDayWeak2.innerText = 'Tuesda';
+    weatherNextDayWeak3.innerText = 'Wensday';
+    weatherTodayInfoListItem1.innerText = 'rain';
+    weatherTodayInfoListItem2.innerText = 'Feels like:  +5°';
+    weatherTodayInfoListItem3.innerText = 'Wind: 8 m/s';
+    weatherTodayInfoListItem4.innerText = 'Humidity: 88%n';
+
+    weatherNextDay1.append(
+      weatherNextDayWeak1,
+      weatherNextDay1Temp,
+      weatherNextDay1Img
+    )
+
+    weatherNextDay2.append(
+      weatherNextDayWeak2,
+      weatherNextDay2Temp,
+      weatherNextDay2Img
+    )
+
+    weatherNextDay3.append(
+      weatherNextDayWeak3,
+      weatherNextDay3Temp,
+      weatherNextDay3Img
+    )
+
+    weatherTodayInfoList.append(
+      weatherTodayInfoListItem1,
+      weatherTodayInfoListItem2,
+      weatherTodayInfoListItem3,
+      weatherTodayInfoListItem4
+    )
 
     weatherToday.append(
       weatherTodayTemp,
-      weatherTodayImg
+      weatherTodayImg,
+      weatherTodayInfoList
     )
 
     weatherNextDayList.append(
