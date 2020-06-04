d924fc748b2aebbd751d64dcfff3ab719b267bf1
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-06-04 14:16:36 +0300

    feat: add function uppdate weatherTodayInfo, updateMap

--- src/components/BuildDom/WeatherToday.js
+++ src/components/BuildDom/WeatherToday.js
@@ -1,7 +1,30 @@
 import { createElement } from '../component/createElement';
 
+const weatherBlock = createElement('div', 'weather_block');
 const weatherCity = createElement('div', 'weather_today_block--city_country', 'text_base');
+const weatherDate =createElement('div', 'text_base', 'weather_today_block--date_time');
+const weatherToday = createElement('div', 'weather-today');
 
+const weatherTodayImg = createElement('img', 'weather_today_block--sign');
+const weatherTodayInfoList = createElement('div', 'weather_today_block--info_list');
+const weatherTodayInfoListWeather = createElement('div', 'text_base', 'weather_today_block--info_list-item');
+const weatherTodayInfoListFeels = createElement('div', 'text_base', 'weather_today_block--info_list-item');
+const weatherTodayInfoListWind = createElement('div', 'text_base', 'weather_today_block--info_list-item');
+const weatherTodayInfoListHumidity = createElement('div', 'text_base', 'weather_today_block--info_list-item');
+const weatherTodayTemp = createElement('div', 'text_base', 'weather_today_block--temp');
+const weatherNextDayList = createElement('div', 'weather_days_block');
+const weatherNextDay1 = createElement('div', 'day_block');
+const weatherNextDayWeak1 = createElement('div', 'text_base', 'day_block--name');
+const weatherNextDay1Temp = createElement('div', 'text_base', 'day_block--temp');
+const weatherNextDay1Img =createElement('img', 'day_block--sign')
+const weatherNextDay2 = createElement('div', 'day_block');
+const weatherNextDayWeak2 = createElement('div', 'text_base', 'day_block--name');
+const weatherNextDay2Temp = createElement('div', 'text_base', 'day_block--temp');
+const weatherNextDay2Img =createElement('img', 'day_block--sign')
+const weatherNextDay3 = createElement('div', 'day_block');
+const weatherNextDayWeak3 = createElement('div', 'text_base', 'day_block--name');
+const weatherNextDay3Temp = createElement('div', 'text_base', 'day_block--temp');
+const weatherNextDay3Img =createElement('img', 'day_block--sign');
 
 export class WeatherToday {
   constructor() {
@@ -9,45 +32,24 @@ export class WeatherToday {
 
   }
 
-  updateData(city) {
+  updateData(city, tempToday, weather, feels, wind, humidity) {
     weatherCity.innerText = city;
+    weatherTodayTemp.innerText = tempToday
+    weatherTodayInfoListWeather.innerText = weather;
+    weatherTodayInfoListFeels.innerText = feels;
+    weatherTodayInfoListWind.innerText = wind;
+    weatherTodayInfoListHumidity.innerText = humidity;
   }
 
   createWeatherTodayBlock(rootElement) {
-    const weatherBlock = createElement('div', 'weather_block');
-    // const weatherCity = createElement('div', 'weather_today_block--city_country', 'text_base');
-    const weatherDate =createElement('div', 'text_base', 'weather_today_block--date_time');
-    const weatherToday = createElement('div', 'weather-today');
-    const weatherTodayTemp = createElement('div', 'text_base', 'weather_today_block--temp');
-    const weatherTodayImg = createElement('img', 'weather_today_block--sign');
-    const weatherTodayInfoList = createElement('div', 'weather_today_block--info_list');
-    const weatherTodayInfoListItem1 = createElement('div', 'text_base', 'weather_today_block--info_list-item');
-    const weatherTodayInfoListItem2 = createElement('div', 'text_base', 'weather_today_block--info_list-item');
-    const weatherTodayInfoListItem3 = createElement('div', 'text_base', 'weather_today_block--info_list-item');
-    const weatherTodayInfoListItem4 = createElement('div', 'text_base', 'weather_today_block--info_list-item');
-    
-    const weatherNextDayList = createElement('div', 'weather_days_block');
-    const weatherNextDay1 = createElement('div', 'day_block');
-    const weatherNextDayWeak1 = createElement('div', 'text_base', 'day_block--name');
-    const weatherNextDay1Temp = createElement('div', 'text_base', 'day_block--temp');
-    const weatherNextDay1Img =createElement('img', 'day_block--sign')
-    const weatherNextDay2 = createElement('div', 'day_block');
-    const weatherNextDayWeak2 = createElement('div', 'text_base', 'day_block--name');
-    const weatherNextDay2Temp = createElement('div', 'text_base', 'day_block--temp');
-    const weatherNextDay2Img =createElement('img', 'day_block--sign')
-    const weatherNextDay3 = createElement('div', 'day_block');
-    const weatherNextDayWeak3 = createElement('div', 'text_base', 'day_block--name');
-    const weatherNextDay3Temp = createElement('div', 'text_base', 'day_block--temp');
-    const weatherNextDay3Img =createElement('img', 'day_block--sign');
-
     weatherCity.innerText = 'Belarus, Minsk';
     weatherDate.innerText = `${this.date.getHours()} : ${this.date.getMinutes()}`;
     weatherTodayTemp.innerText = '+54';
     weatherTodayImg.setAttribute('src', 'assets/img/rain.png');
-    weatherTodayInfoListItem1.innerText = 'rain';
-    weatherTodayInfoListItem2.innerText = 'Feels like:  +5°';
-    weatherTodayInfoListItem3.innerText = 'Wind: 8 m/s';
-    weatherTodayInfoListItem4.innerText = 'Humidity: 88%n';
+    weatherTodayInfoListWeather.innerText = 'rain';
+    weatherTodayInfoListFeels.innerText = 'Feels like:  +5°';
+    weatherTodayInfoListWind.innerText = 'Wind: 8 m/s';
+    weatherTodayInfoListHumidity.innerText = 'Humidity: 88%n';
 
     weatherNextDay1Img.setAttribute('src', 'assets/img/rain.png');
     weatherNextDay2Img.setAttribute('src', 'assets/img/rain.png');
@@ -85,10 +87,10 @@ export class WeatherToday {
     )
 
     weatherTodayInfoList.append(
-      weatherTodayInfoListItem1,
-      weatherTodayInfoListItem2,
-      weatherTodayInfoListItem3,
-      weatherTodayInfoListItem4
+      weatherTodayInfoListWeather,
+      weatherTodayInfoListFeels,
+      weatherTodayInfoListWind,
+      weatherTodayInfoListHumidity
     )
 
     weatherToday.append(
