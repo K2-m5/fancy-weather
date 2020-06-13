3efd420ac9e1e898d229ffdc732c479759484ea7
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-06-13 14:13:05 +0300

    refactor: fixed errors eslint

--- src/components/BuildDom/MapBlock.js
+++ src/components/BuildDom/MapBlock.js
@@ -1,34 +1,30 @@
 import { createElement } from '../component/createElement';
-import mapboxgl from 'mapbox-gl'
+import mapboxgl from 'mapbox-gl';
 
 export class MapBlock {
   constructor(mapKey) {
     this.mapboxGl = mapboxgl;
     this.map = null;
-
+    this.mapBlock = createElement('div', 'map-block');
     this.mapboxGl.accessToken = mapKey;
   }
 
   createMapBlock() {
-    const mapBlock = createElement ('div', 'map-block');
-    const map = createElement ('div', 'map');
+    const map = createElement('div', 'map');
     map.id = 'map';
-
-    mapBlock.append(map);
-
-    return mapBlock;
+    this.mapBlock.append(map);
+    return this.mapBlock;
   }
 
   addMap(lon, lat) {
-      this.map = new mapboxgl.Map({
-          container: 'map',
-          style: 'mapbox://styles/mapbox/dark-v10',
-          center: [lon, lat],
-          zoom: 12
-      });
-
-      return(map);
-    };
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [lon, lat],
+      zoom: 12
+    });
+    return (this.map);
+  }
 
   updateMap(lon, lat) {
     if (!this.map) return;
@@ -38,5 +34,4 @@ export class MapBlock {
       zoom: 12
     });
   }
-
 }
