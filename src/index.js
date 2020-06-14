d65b227964d35240a0c24a4ef40e7a9e06869858
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-06-14 12:02:23 +0300

    refactor: delete unuse code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import { BuildDom } from './components/BuildDom/BuildDom';
-import { openWeatherKeys } from './components/const';
-import { WeatherFetch } from './components/FetchApp/WeatherFetch';
-import { CoordUserFetch } from './components/FetchApp/CoordUserFetch';
 
 import './styles.css';
 
-const buildDom = new BuildDom(document.body, openWeatherKeys);
-const weather = new WeatherFetch(openWeatherKeys);
-
-const data = weather.getCurrentWeatherByCity('Minsk');
-console.log(data);
+const buildDom = new BuildDom(document.body);
 
 buildDom.buildDom();
-
-
