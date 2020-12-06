e42575fdf7217ba8b76a49cfa07430d89a9abdad
Pavel Svirid <56111100+K2-m5@users.noreply.github.com>
2020-12-06 23:56:32 +0300

    feat: function getPaceData add new param language

--- src/components/FetchApp/PlaceCoordinateApi.js
+++ src/components/FetchApp/PlaceCoordinateApi.js
@@ -6,11 +6,12 @@ export default class PlaceCoordinateApi extends Api {
     this.apiKeys = {
       TOKEN: '&key=ee04ecdb632d47b0b1fc6b541148a06e',
       URL: 'https://api.opencagedata.com/geocode/v1/json?q=',
+      PARAMS: '',
     };
   }
 
-  async getCoordinateByPlace(city) {
-    const url = this.apiKeys.URL + city + this.apiKeys.TOKEN;
+  async getCoordinateByPlace(city, lang) {
+    const url = `${this.apiKeys.URL + city + this.apiKeys.TOKEN}&language=${lang}`;
     const data = await this.getJsonData(url);
     if (data.results.length === 0) {
       return false;
@@ -18,9 +19,9 @@ export default class PlaceCoordinateApi extends Api {
     return data;
   }
 
-  async getInfoByCity(query, lang) {
+  async getInfoByCoords(lat, lon, lang) {
     const url = `${
-      this.apiKeys.URL + query + this.apiKeys.KEY + this.apiKeys.PARAMS
+      this.apiKeys.URL + lat + lon + this.apiKeys.KEY + this.apiKeys.PARAMS
     }&language=${lang}`;
     const data = await this.getJsonData(url);
     return data;
